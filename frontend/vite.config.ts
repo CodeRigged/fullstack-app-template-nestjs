4abb55765195c5d1cdc4eedb6c6d534afeee44e2
CodeRigged <tantonovics@gmail.com>
2026-02-16 13:54:32 +0100

    feat(chore): add Docker support for backend and frontend, including .dockerignore and docker-compose.yml

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  preview: {
+    host: true,
+    port: 8080,
+  },
   server: {
     port: 9000,
   },
