c7a84c8ffbe788d8c599632453913a7b1d485801
CodeRigged <tantonovics@gmail.com>
2026-02-18 12:11:14 +0100

    refactor(backend): remove dotenv and replace with @nestjs/config and joi for env validation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from "@nestjs/common"
+import { ConfigModule } from "@nestjs/config"
 import { MongooseModule } from "@nestjs/mongoose"
-import * as dotenv from "dotenv"
+import Joi from "joi"
 import { LoggerModule } from "nestjs-pino"
 
 import { HealthModule } from "./base/health.module.js"
 import { loggerOptions } from "./config/logger.js"
 import { TodoModule } from "./models/todo/todo.module.js"
-
-dotenv.config()
 
 /**
  * The root module of the application.
@@ -15,8 +14,14 @@ dotenv.config()
  */
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+      }),
+    }),
     LoggerModule.forRoot(loggerOptions),
-    MongooseModule.forRoot(process.env.MONGO_URI || "mongodb://localhost:27017/todos"),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
     HealthModule,
     TodoModule,
   ],
